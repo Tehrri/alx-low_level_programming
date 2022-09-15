3dfd9fb629ae4f71356d1b5238750419035fe813
Tehrri <tehrrignyaga@gmail.com>
2022-09-15 06:52:26 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main -print the string in the print function
  * 
- * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art"
+ * Description:using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0 (Success)
  */
 int main (void)
-{    
-printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
