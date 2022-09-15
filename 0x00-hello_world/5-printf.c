e0d22ed51dffa2e75326d677fa64a9f8199b0546
Tehrri <tehrrignyaga@gmail.com>
2022-09-15 06:55:29 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * main -print the string in the print function
  * 
  * Description:using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * this program prints "with proper grammar, but the outcome is a piece of art"
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
