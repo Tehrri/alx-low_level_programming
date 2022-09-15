e4fbc5c4eace33e487df426d765cf143a5623510
Tehrri <tehrrignyaga@gmail.com>
2022-09-15 07:47:08 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main-prints exactly "and that piece of art is useful"
+ * main - Entry point
+ *
+ * Dexcription: prints exactly "and that piece of art is useful"
  * -Dora Korpar, 2015-10-19",
- *  Return: Always 0 (Success)
+ *
+ *  Return: This time we return an error 1.
  */
+
 int main(void)
 {
-	write(2, and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+       char quo[] ="and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+
+       write(1, quo, 59);
 	Return (1);
 }
