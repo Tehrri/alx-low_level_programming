28eef28263552497777436ce7eaaba774f39e02b
Tehrri <tehrrignyaga@gmail.com>
2022-09-22 10:22:47 -0700

    My c programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /**
- * Main : This is s program that prints alphabets in lowecase
+ * Main - program that prints alphabets in lowecase
+ *
  * Return : Always 0
  */
 
 int main(void)
 {
 	int a;
+
 	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
 	putchar ('\n');
+
 	return (0);
 }
 
