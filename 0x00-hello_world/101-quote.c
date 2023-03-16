71859110d148dabd37738878d9ad52913a9358bb
tehrri <tehrrignyaga@gmail.com>
2023-03-16 03:40:33 -0700

    the useful art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Dexcription: prints exactly "and that piece of art is useful"
- * -Dora Korpar, 2015-10-19",
+ * Description: this program prints exactly "and that piece of art is useful"
+ * ssize_t write(int fd, const void *buf, size_t count);
  *
  *  Return: This time we return an error 1.
  */
