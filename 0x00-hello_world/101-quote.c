f98cc235ccf2665cf88202da4f0c9671c86801ec
tehrri <tehrrignyaga@gmail.com>
2023-03-20 10:25:52 -0700

    and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  * Description: this program prints exactly "and that piece of art is useful"
  * ssize_t write(int fd, const void *buf, size_t count);
  *
- *  Return: Always 1.
+ * Return: this time we return 1 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return (1)
+	return (1);
 }	
