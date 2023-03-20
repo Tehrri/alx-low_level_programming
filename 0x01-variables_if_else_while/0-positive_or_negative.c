e1389b19dda2f0c1eb5a02e566ef5fdbfcb1cc30
tehrri <tehrrignyaga@gmail.com>
2023-03-20 11:23:44 -0700

    print whether number is +ve or _ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - entry point
  *
  * Description: prints if a number is positive or negative 
- * Return: 0
+ * Return: 0 (Success
  */
 
 int main(void)
 {
 	int n;
 
-	srand(time(o));
-	n = rand() - RAND-MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
