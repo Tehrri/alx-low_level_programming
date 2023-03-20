db79fe9d18a3a61dcfc7d7fada31facf4237536e
tehrri <tehrrignyaga@gmail.com>
2023-03-20 11:30:00 -0700

    print whether number is +ve or _ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,9 @@
 /**
  * main - entry point
  *
- * Description: prints if a number is positive or negative 
- * Return: 0 (Success
+ * Description: prints if a number is positive or negative
+ *
+ * Return: Always 0 (Success
  */
 
 int main(void)
@@ -17,16 +18,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+		printf("%d is positive\n", n);	
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
+
 	return (0);
 }
