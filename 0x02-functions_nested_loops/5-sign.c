5f7b4e77841a3ba73e178414dff3d9d3a7b94c55
tehrri <tehrrignyaga@gmail.com>
2023-03-26 07:25:40 -0700

    fixing betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - print + if n is greater than zero,0 if n is zero and - if n is less than 0
+ * print_sign - print + if n is greaterthan 0,0 if n is 0, - if n is lessthan 0
  *
  * @n: takes integer type input for function
  *
