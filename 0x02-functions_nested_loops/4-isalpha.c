2e4e1f44cbf4f28988319e86f1157f6a938e53e7
tehrri <tehrrignyaga@gmail.com>
2023-03-26 07:12:44 -0700

    checks whether leter is lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a charcter is lower case or upper case
+ *
+ * @c: unsigned int value to be compared with ASCII value
+ *
+ * Return: 1 if c is true else 0
+ */
+
+int _isalpha(int c)
+{
+	if(c >=65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
