737670b869e4b115fba987827944eb8a8089c0d5
tehrri <tehrrignyaga@gmail.com>
2023-03-27 10:12:52 -0700

    write a function that returns the lenth of a string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,7 +6,7 @@
  * @a: a pointer dereference for changing the value of the variable a
  * @b: a pointer deference for changing the value of the variable b
  * int c: assigning the address of the variable a to c
- * 
+ *
  * Return 0 success.
 */
 
